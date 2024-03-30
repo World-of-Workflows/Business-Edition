329ce6ac22233c53ecb37394076a244ac7ca56ac
Nick Beaugeard <nick@tribetech.com.au>
2024-03-30 16:54:32 +1100

    Added Server and Secrets

--- Deployment/ADDeploymentScript.ps1
+++ Deployment/ADDeploymentScript.ps1
@@ -21,8 +21,49 @@ $redirectUris = @(
 # Assuming $ClientappName and $redirectUris are predefined
 $ClientApp = New-AzADApplication -DisplayName $ClientappName -SPARedirectUri $redirectUris -AvailableToOtherTenants $false
 
+Connect-AzureAd
+
+$graphSp=Get-AzureADServicePrincipal -Filter "displayName eq 'Microsoft Graph'"
+$userReadId = $graphSp.Oauth2Permissions| Where-Object { $_.Value -eq 'User.Read' } | Select-Object -ExpandProperty Id
+$userReadAllId = $graphSp.Oauth2Permissions | Where-Object { $_.Value -eq 'User.ReadBasic.All' } | Select-Object -ExpandProperty Id
+
+$requiredPermissions = @(
+    # User.Read
+    @{
+        "ResourceAppId" = "00000003-0000-0000-c000-000000000000"
+        "ResourceAccess" = @(
+            @{
+                "Id" = $userReadId 
+                "Type" = "Scope"
+            }
+        )
+    }
+)
+Update-AzAdApplication -ObjectId $ClientApp.Id -RequiredResourceAccess $requiredPermissions
+
+$requiredPermissions = @(
+    # User.Read
+    @{
+        "ResourceAppId" = "00000003-0000-0000-c000-000000000000"
+        "ResourceAccess" = @(
+            @{
+                "Id" = $userReadAllId 
+                "Type" = "Scope"
+            }
+        )
+    }
+)
+Update-AzAdApplication -ApplicationId $ClientApp.Id -RequiredResourceAccess $requiredPermissions
+
+$ServerApp = New-AzAdApplication -DisplayName $ServerappName -SignInAudience "AzureADMyOrg"
+$ServerSecret = New-AzureAdApplicationPasswordCreden
+tial -ObjectId $ServerApp.Id -CustomKeyIdentifier PrimarySecret -EndDate ((Get-Date).AddMonths(12))
+
+
 $DeploymentScriptOutputs = @{}
 $DeploymentScriptOutputs['ClientClientId'] = $ClientApp.AppId
+$DeploymentScriptOutputs['ServerClientId'] = $ServerApp.AppId
+$DeploymentScriptOutputs['ServerSecret'] = $ServerSecret.Value
 
 
 
